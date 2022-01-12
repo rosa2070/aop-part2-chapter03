d2c3a7cf3770209bce7ab45ecc3ba73134e03c87
jaeeunhyun <rosa2070@naver.com>
2022-01-13 01:30:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aop_part2_chapter03/DiaryActivity.kt
@@ -0,0 +1,14 @@
+package com.example.aop_part2_chapter03
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DiaryActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+
+
+    }
+}
